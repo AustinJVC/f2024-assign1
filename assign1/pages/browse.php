41db2a2d1b8c5046681013dd9328e8f8b7ca983d
AustinJVC <avand955@mtroyal.ca>
2024-10-16 13:09:09 -0600

    added template for browse

---
+++ assign1/pages/browse.php
@@ -0,0 +1,31 @@
+<<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+       <header>
+            <h1> F1 Dashboard Project </h1>
+            <a href='home.php'>Home</a>
+            <a href='browse.php'>Browse</a>
+            <a href='apis.php'>APIs</a>
+        </header>
+        <sidebar>
+<!--
+    Side Content
+-->
+        </sidebar>
+        <div class='content'>
+        <!--
+    Main center content goes here
+-->
+
+        </div>
+    </body>
+</html>
