f4f7bfe4bc9515387cdd9cff91468e170c84c9d5
AustinJVC <avand955@mtroyal.ca>
2024-10-16 23:00:44 -0600

    results half working

--- assign1/api/results.php
+++ assign1/api/results.php
@@ -5,3 +5,27 @@ Don’t provide the foreign keys for the race, driver, and constructor; instead 
 Sort by the field grid in ascending order (1st place first, 2nd place second, etc).
 /api/results.php?driver=?
 Returns all the results for a given driver, e.g., /api/results/driver/max_verstappen
+
+
+<?php
+include 'db.inc.php';
+
+function getRaceResults($raceId){
+        $results = getData("SELECT drivers.driverRef, drivers.code, drivers.forename, drivers.surname,races.name, races.round, races.year, races.date,constructors.name, constructors.constructorRef, constructors.nationality FROM results JOIN races ON results.raceId = races.raceId JOIN drivers ON results.driverId = drivers.driverId JOIN constructors ON results.constructorId = constructors.constructorId   WHERE races.raceId=? ORDER BY results.grid ASC", [$raceId]);
+
+        echo json_encode($results); 
+}
+function getDriverResults($driver){
+    $results = getData("SELECT", [$driver]);
+
+        echo json_encode($results); 
+}
+
+if(isset($_GET['ref'])){
+    getRaceResults($_GET['ref']);
+}elseif (isset($_GET['driver'])){
+    getDriverResults($_GET['driver']);
+}
+
+
+?>
